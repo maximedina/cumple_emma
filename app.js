77f43481609f898d779bfb75efba710350654392
Medina <maximiliano.medina@arcelormittal.com.ar>
2021-11-04 19:46:14 -0300

    f

--- app.js
+++ app.js
@@ -2,41 +2,39 @@ var tiempo;
 var temporizador;
 
 window.onload = function () {
-  temp=localStorage.getItem("tiempo")
-  if (temp!=0 && temp!=null && !isNaN(temp)) 
-    {
-      tiempo = temp;
-      frm = document.forms['formulario'];
-      for(i=0; ele=frm.elements[i]; i++)
-        {ele.disabled=true;}
-      temporizador=setInterval(habilitar, 1000);
+  temp = localStorage.getItem("tiempo");
+  if (temp != 0 && temp != null && !isNaN(temp)) {
+    tiempo = temp;
+    frm = document.forms["formulario"];
+    for (i = 0; (ele = frm.elements[i]); i++) {
+      ele.disabled = true;
     }
-}
+    temporizador = setInterval(habilitar, 1000);
+  }
+};
 function seleccion() {
-if (document.formulario.opcion.value == "J") {
-  document.getElementById("Pista2").style.display = "inline-flex";
-
-}
-else {
-  tiempo=10;
-  frm = document.forms['formulario'];
-  for(i=0; ele=frm.elements[i]; i++)
-    {ele.disabled=true;}
-  timer = setInterval(habilitar, 1000);
-}
+  if (document.formulario.opcion.value == "J") {
+    document.getElementById("pista2").style.display = "inline-flex";
+  } else {
+    tiempo = 10;
+    frm = document.forms["formulario"];
+    for (i = 0; (ele = frm.elements[i]); i++) {
+      ele.disabled = true;
+    }
+    timer = setInterval(habilitar, 1000);
+  }
 }
 
 function habilitar() {
   tiempo--;
   localStorage.setItem("tiempo", tiempo);
-  if (tiempo<=0)
-  {
-  frm = document.forms['formulario'];
-  for(i=0; ele=frm.elements[i]; i++)
-    {ele.disabled=false;}
-  clearInterval(temporizador);
+  if (tiempo <= 0) {
+    frm = document.forms["formulario"];
+    for (i = 0; (ele = frm.elements[i]); i++) {
+      ele.disabled = false;
+    }
+    clearInterval(temporizador);
   }
-
 }
 
 /*
