3e9c36e4d37c074d8ca3db25cb48dfb04a280074
Medina <maximiliano.medina@arcelormittal.com.ar>
2021-11-04 18:41:39 -0300

    d

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
+var tiempo;
+var temporizador;
+
+window.onload = function () {
+  temp=localStorage.getItem("tiempo")
+  if (temp!=0 && temp!=null && !isNaN(temp)) 
+    {
+      tiempo = temp;
+      frm = document.forms['formulario'];
+      for(i=0; ele=frm.elements[i]; i++)
+        {ele.disabled=true;}
+      temporizador=setInterval(habilitar, 1000);
+    }
+}
 function seleccion() {
 if (document.formulario.opcion.value == "J") {
   document.getElementById("Pista2").style.display = "inline-flex";
 
 }
 else {
+  tiempo=10;
   frm = document.forms['formulario'];
   for(i=0; ele=frm.elements[i]; i++)
     {ele.disabled=true;}
-  timer = setInterval(habilitar, 5000);
+  timer = setInterval(habilitar, 1000);
 }
 }
 
 function habilitar() {
+  tiempo--;
+  localStorage.setItem("tiempo", tiempo);
+  if (tiempo<=0)
+  {
   frm = document.forms['formulario'];
   for(i=0; ele=frm.elements[i]; i++)
     {ele.disabled=false;}
-  clearInterval(timer);
+  clearInterval(temporizador);
+  }
+
 }
 
 /*
